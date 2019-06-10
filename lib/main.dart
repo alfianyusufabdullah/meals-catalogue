2074e5a9a3f6da6980333175a223dfac00bf3d2b
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-10 21:50:31 +0700

    change root app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meals_catalogue/ui/meals_list.dart';
+import 'package:meals_catalogue/ui/meals_home.dart';
 
-void main() => runApp(MealsList());
+void main() => runApp(MealsHome());
