9b595757078514031d33e9f57f6733b18f45a4ac
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-10 21:50:57 +0700

    add TagsMeal as Widget

--- lib/common/meals_common.dart
+++ lib/common/meals_common.dart
@@ -46,3 +46,42 @@ class CustomList extends StatelessWidget {
     );
   }
 }
+
+class TagsMeal extends StatelessWidget {
+  final List<String> tags;
+
+  const TagsMeal({Key key, this.tags}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.horizontal,
+      children: tags.map((item) {
+        return Padding(
+          padding: EdgeInsets.only(left: 3.0, right: 3.0),
+          child: Chip(
+            label: Text(
+              item,
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue,
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class SliceText extends StatelessWidget {
+  final String text;
+
+  const SliceText({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text.length > 14 ? "${text.substring(0, 13)}..." : text,
+      style: TextStyle(color: Colors.black, fontSize: 17),
+    );
+  }
+}
