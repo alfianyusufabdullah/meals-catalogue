2ec98f17eb0bdfa253e0957d91cab911c3cfb051
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-13 10:59:38 +0700

    initial favorite page

---
+++ lib/ui/meals_favorite.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MealsFavorite extends StatefulWidget {
+  @override
+  _MealsFavoriteState createState() => _MealsFavoriteState();
+}
+
+class _MealsFavoriteState extends State<MealsFavorite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
