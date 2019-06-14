a0792335517f5a14e9b1468a025182710e784c8f
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-14 14:11:50 +0700

    add function for search meal

--- lib/data/meals_data.dart
+++ lib/data/meals_data.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:meals_catalogue/data/meals_data_mapper.dart';
+import 'package:meals_catalogue/database/meals_database.dart';
 import 'package:meals_catalogue/model/meals.dart';
 
 Future<List<Meals>> loadMealsFromNetwork(String category) async {
@@ -16,4 +17,16 @@ Future<Meals> loadMealsDetailFromNetwork(String id) async {
       .then((response) => generateResponse(response))
       .then((json) => generateList(json))
       .then((meals) => generateMeal(meals));
+}
+
+Future<Meals> searchMealFromNetwork(String name) async {
+  return await http
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s$name")
+      .then((response) => generateResponse(response))
+      .then((json) => generateList(json))
+      .then((meals) => generateMeal(meals));
+}
+
+Future<List<Meals>> loadMealsFromLocal(DatabaseHelper helper,String category) async{
+  return await helper.getMealFavorite(category);
 }
