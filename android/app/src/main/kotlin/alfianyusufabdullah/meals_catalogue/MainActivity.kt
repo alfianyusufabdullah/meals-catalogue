e7c11791d8ebaf171d53601e195da8bc73c19844
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-14 14:09:03 +0700

    override onBackPressed

--- android/app/src/main/kotlin/alfianyusufabdullah/meals_catalogue/MainActivity.kt
+++ android/app/src/main/kotlin/alfianyusufabdullah/meals_catalogue/MainActivity.kt
@@ -5,9 +5,17 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+
+    override fun onBackPressed() {
+        if (flutterView != null){
+            flutterView.popRoute()
+            return
+        }
+        super.onBackPressed()
+    }
 }
