99d0a6c2894358d8bb964e5d1003f577daf881b3
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-06-16 21:05:52 +0700

    create network layer

--- lib/data/meals_data.dart
+++ lib/data/meals_data.dart
@@ -2,31 +2,24 @@ import 'package:http/http.dart' as http;
 import 'package:meals_catalogue/data/meals_data_mapper.dart';
 import 'package:meals_catalogue/database/meals_database.dart';
 import 'package:meals_catalogue/model/meals.dart';
+import 'package:meals_catalogue/network/meals_network.dart';
 
 Future<List<Meals>> loadMealsFromNetwork(String category) async {
-  return await http
-      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=$category")
-      .then((response) => generateResponse(response))
-      .then((json) => generateList(json))
-      .then((meals) => generateMealList(meals));
+  var result = await httpRequest("$ENDPOINT_FILTER$category");
+  return generateMealList(result);
 }
 
 Future<Meals> loadMealsDetailFromNetwork(String id) async {
-  return await http
-      .get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=$id")
-      .then((response) => generateResponse(response))
-      .then((json) => generateList(json))
-      .then((meals) => generateMeal(meals));
+  var result = await httpRequest("$ENDPOINT_LOCKUP$id");
+  return generateMeal(result);
 }
 
-Future<Meals> searchMealFromNetwork(String name) async {
-  return await http
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s$name")
-      .then((response) => generateResponse(response))
-      .then((json) => generateList(json))
-      .then((meals) => generateMeal(meals));
+Future<List<Meals>> searchMealFromNetwork(String name) async {
+  var result = await httpRequest("$ENDPOINT_SEARCH$name");
+  return generateMealList(result);
 }
 
-Future<List<Meals>> loadMealsFromLocal(DatabaseHelper helper,String category) async{
+Future<List<Meals>> loadMealsFromLocal(
+    DatabaseHelper helper, String category) async {
   return await helper.getMealFavorite(category);
 }
